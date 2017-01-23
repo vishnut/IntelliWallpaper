b107c66da5d0170a61a68327012ab0d5662919e5
Vishnu Thiagarajan <thiagarajanvishnu@gmail.com>
2017-01-23 13:20:13 -0600

    trying to fix apple wallpaper set issue

--- natgeowall.sh
+++ natgeowall.sh
@@ -8,6 +8,7 @@
 
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
+FILENAME="bgrd"
 
 cd $BASEDIR
 
@@ -20,15 +21,29 @@ grep -i -h -o "http://yourshot.nationalgeographic.com/u/.*./" images/doc.txt > i
 saveURL=`grep -m 1 -v "\"" images/docurl.txt`
 echo "Found Image URL: $saveURL"
 
-curl -o images/bgrd.jpg "$saveURL"
-echo "Image Saved as images/bgrd.jpg"
+curl -o images/"$FILENAME".jpg "$saveURL"
+echo "Image Saved as images/${FILENAME}.jpg"
 
 rm images/doc.txt
 rm images/docurl.txt
 echo "Removed temp files"
 
 cwdText=`pwd`
-saveLoc="${cwdText}/images/bgrd.jpg"
+
+#Clear first
+saveLo="${cwdText}/images/black.jpg"
+setScrip="tell application \"System Events\"
+    set desktopCount to count of desktops
+    repeat with desktopNumber from 1 to desktopCount
+        tell desktop desktopNumber
+            set picture to \"${saveLo}\"
+        end tell
+    end repeat
+end tell"
+
+osascript -e "$setScrip"
+
+saveLoc="${cwdText}/images/${FILENAME}.jpg"
 echo $saveLoc
 
 #setScript="tell application \"Finder\" to set desktop picture to \"${saveLoc}\" as POSIX file" 
